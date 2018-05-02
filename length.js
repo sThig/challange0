f274caf15bf1a6624ffd7a54abf96fe0e4b1351d
sthig <scott.thigpen@gmail.com>
2018-05-02 11:45:58 -0500

    justin helped complete this one

---
+++ length.js
@@ -0,0 +1,23 @@
+function longestWord(sentence) {
+  var str = sentence.split(' ');
+  var longest = 0;
+  var word = null;
+  for (var i = 0; i <= str.length - 1; i++) {
+    if (longest < str[i].length) {
+      longest = str[i].length;
+      word = str[i];
+    }
+  }
+  return word;
+}
+
+longestWord('The quick brown fox jumped over the lazy dog');
+
+//better
+
+function longestWord(sentence) {
+  return sentence.split(' ').sort(sortByLength)[0];
+}
+
+function sortByLength(a, b) {
+  return b.length - a.length;
